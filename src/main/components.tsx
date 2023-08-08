103faceb872eec23967456896c6e35bda793a3d4
Bianca Cibotaru <bianca.cibotaru@indsoft.ro>
2023-08-08 20:57:32 +0300

    Add TeamsTable component

--- src/main/components.tsx
+++ src/main/components.tsx
@@ -1,3 +1,18 @@
+import { TeamsTables } from "../teams/TeamsTable";
+
 export function ContentWrapper() {
-  return <div id="main">TODO table...</div>;
+  return (
+    <div id="main">
+      <div className="tbar">
+        <button id="removeSelected">❌ Remove selected</button>
+        <div>
+          <input type="search" name="search" id="searchTeams" placeholder="Search..." />
+          <label htmlFor="searchTeams">🔎</label>
+        </div>
+      </div>
+      <TeamsTables loading={false} />
+      <br />
+      <TeamsTables loading={true} />
+    </div>
+  );
 }
