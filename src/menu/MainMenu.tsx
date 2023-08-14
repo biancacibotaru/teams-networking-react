013a38cc9745ff33570d8cad56052ac78393d3bd
Bianca Cibotaru <bianca.cibotaru@indsoft.ro>
2023-08-14 20:48:02 +0300

    Use setActive hook to store current page

--- src/menu/MainMenu.tsx
+++ src/menu/MainMenu.tsx
@@ -5,10 +5,11 @@ import { Page } from "../main/models";
 type Props = {
   activePage: Page;
 };
+type Actions = {
+  setActive(active: Page): void;
+};
 
-export function MainMenu(props: Props) {
-  const [active, setActive] = useState<Page>(props.activePage);
-
+export function MainMenu(props: Props & Actions) {
   const elements = [
     { text: "Home", name: "home" },
     { text: "Todos", name: "todos" },
@@ -21,9 +22,9 @@ export function MainMenu(props: Props) {
         <li key={element.name}>
           <a
             href={"#" + element.name}
-            className={active === element.name ? "active" : ""}
+            className={props.activePage === element.name ? "active" : ""}
             onClick={() => {
-              setActive(element.name as Page); //tmp use 'as Page'
+              props.setActive(element.name as Page); //tmp use 'as Page'
             }}
           >
             {element.text}
